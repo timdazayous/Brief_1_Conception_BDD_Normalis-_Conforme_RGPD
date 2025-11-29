7f34ffaa25150fff8d271957df0164ec7295384b
timdazayous <tim.dazayous@gmail.com>
2025-11-30 00:50:51 +0100

    Classes Corrected

--- class/class.py
+++ class/class.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy import Column, Integer, String, ForeignKey, Boolean
 from sqlalchemy.orm import declarative_base, relationship
 
 Base = declarative_base()
@@ -10,11 +10,11 @@ class User(Base):
     pseudo_user = Column(String, nullable=False)
     password_user = Column(String,nullable=False)
     mail_user = Column(String,nullable=False, unique=True) # à crypter
-    consent_user = Column(bool,nullable=False)
+    consent_user = Column(Boolean,nullable=False)
     limitdate_user = Column(Integer, nullable=False)
     # relation avec les autres tables
     # avec table d'association Users_Games
-    games = relationship('User_Game', back_populates='user')
+    games_links = relationship('User_Game', back_populates='user')
 
 class Game(Base):
     __tablename__ = 'Games'
@@ -25,17 +25,17 @@ class Game(Base):
     # avec table de fait Sales
     sales = relationship('Sale', back_populates='game')
     # avec table d'association Users_Games
-    users_games = relationship('User_Game', back_populates='game')
+    users_links = relationship('User_Game', back_populates='game')
     # avec la table d'association Games_Platforms
-    games_platforms = relationship('Game_Platform', back_populates='game')
+    platforms_links = relationship('Game_Platform', back_populates='game')
     # avec la table de dimensions Genres
     # clé etrangère venant de Genres
     id_genre_fk = Column(Integer, ForeignKey('Genres.id_genre'))
-    genres = relationship('Genre', back_populates='game')
+    genre = relationship('Genre', back_populates='games')
     # avec la table de dimensions Publishers
     # clé etrangère venant de Publishers
     id_publisher_fk = Column(Integer, ForeignKey('Publishers.id_publisher'))
-    publishers = relationship('Publisher', back_populates='game')
+    publishers = relationship('Publisher', back_populates='games')
 
 ## to be continued
 
@@ -49,22 +49,22 @@ class Sale(Base):
     Global_sale = Column(Integer, nullable=True)
     # relation avec Games
     # clé etrangere 
-    id_game_fk = Column(Integer, ForeignKey('Games.id_game'))
-    games = relationship('Game', back_populates='sale')
+    id_game_fk = Column(Integer, ForeignKey('Games.id_game'), nullable=False)
+    game = relationship('Game', back_populates='sales')
 
 class Publisher(Base):
     __tablename__= 'Publishers'
     id_publisher = Column(Integer, primary_key=True)
     name_publisher = Column(String, nullable=False)
-    # relation avec Games
-    games = relationship('Game', back_populates='publisher')
+    # relation 1,n avec Games 
+    games = relationship('Game', back_populates='publishers')
 
 class Platform(Base):
     __tablename__= 'Platforms'
-    id = Column(Integer,primary_key=True)
-    name = Column(String, nullable=False)
+    id_platform = Column(Integer,primary_key=True)
+    name_platform = Column(String, nullable=False)
     # relation avec Games_Plastforms
-    games_platforms = relationship('Game_platform', back_populates='platform')
+    games_links = relationship('Game_Platform', back_populates='platform')
 
 class Genre(Base):
     __tablename__= 'Genres'
@@ -76,20 +76,20 @@ class Genre(Base):
 class User_Game(Base):
     __tablename__ = 'Users_Games'
     # clés étrangères et primaires
-    id_user_pk_fk = Column(Integer, ForeignKey('Users.id_user'), primary_key=True)
-    id_game_pk_fk = Column(Integer, ForeignKey('Games.id_game'), primary_key=True)
+    id_user = Column(Integer, ForeignKey('Users.id_user'), primary_key=True)
+    id_game = Column(Integer, ForeignKey('Games.id_game'), primary_key=True)
     # relation avec les autres tables
     # avec Games
-    games = relationship('Game', back_populates='user_game')
+    game = relationship('Game', back_populates='users_links')
     # avec Users
-    users = relationship('User', back_populates='user_game')
+    user = relationship('User', back_populates='games_links')
 
 class Game_Platform(Base):
     __tablename__ = 'Games_Platforms'
     release_year = Column(Integer, nullable=True)
     # clés étrangères et primaires
-    id_game_pk_fk = Column(Integer, ForeignKey('Games.id_game'), primary_key=True)
-    id_platform_pk_fk = Column(Integer, ForeignKey('Platforms.id_platform'), primary_key=True)
+    id_game = Column(Integer, ForeignKey('Games.id_game'), primary_key=True)
+    id_platform = Column(Integer, ForeignKey('Platforms.id_platform'), primary_key=True)
     # relation avec les autres tables
-    games = relationship('Game', back_populates='game_platform')
-    Platform = relationship('Platform', back_populates='game_platform')
+    game = relationship('Game', back_populates='platforms_links')
+    platform = relationship('Platform', back_populates='games_links')
