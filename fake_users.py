960874f6fcf4c35a7da494f6051e16840be81110
timdazayous <tim.dazayous@gmail.com>
2025-12-01 17:21:22 +0100

    CRUD User almost done

---
+++ fake_users.py
@@ -0,0 +1,73 @@
+import random
+from faker import Faker
+from sqlalchemy.exc import SQLAlchemyError
+from database import Session
+import models
+
+# liste des localités possible pour diversifier les utilisateurs et etre un peu plus representatif de la realité 
+LOCALES = ['fr_FR', "en_US", "ja_JP", "de_DE", "es_ES", "it_IT", "pt_BR"]
+
+def generate_fake_user():
+    locale = random.choice(LOCALES)
+    fake = Faker(locale)
+
+    return {
+        "pseudo_user": fake.user_name(),
+        "password_user": fake.password(length=10),
+        "mail_user": fake.unique.email(),
+        "consent_user": fake.boolean(chance_of_getting_true=85), # taux d'acceptation du consentement ici 85%
+        "limitdate_user": int(fake.date_between(start_date='+1y', end_date='+3y').strftime("%Y%m%d")) # genere une date limite entre +1 an et +3 ans aleatoirement
+    }
+
+# ajout de fake users avec des liens randomisés avec Game
+def create_users_and_links(nb_users=50, max_games_per_user=5): # par défaut 50 joueurs possédant de 0 a 5 jeux
+    session = Session()
+    try:
+        # on recupere tous les jeux depuis la class Game
+        all_games = session.query(models.Game).all()
+        # on verifie qu'il y a bien des jeux dans la base de données
+        if not all_games:
+            print('Pas de jeux enregistrés') # apres chargment du csv on ne devrait jamais avoir ce probleme
+            return
+        
+        # on va tracker le nb de users associés a des jeux pour pouvoir afficher quelques details lors de la generation des faux utilisateurs
+        users_with_links = 0
+
+        for i in range(nb_users):
+            user_data = generate_fake_user()
+            user = models.User(**user_data)
+            session.add(user)
+            session.flush()
+
+            # on attribut aleatoirement le nombre de jeux de l'utilisateur
+            nb_jeux = random.randint(0, max_games_per_user)
+
+            if nb_jeux > 0:
+                jeux_choisis = random.sample(all_games, k=min(nb_jeux, len(all_games)))
+
+                # création des liens entre User et Game
+                for game in jeux_choisis:
+                    link = models.User_Game(id_user=user.id_user, id_game=game.id_game) # On créer les fk id_game et if_user dans Game_User
+                    session.add(link)
+                users_with_links += 1 # ajout d'un joueur avec au moins 1 jeu
+                print(f'Utilisateur : {user.pseudo_user} lié à {nb_jeux} jeu(x)')
+            else:
+                print(f"Utilisateur {user.pseudo_user} n'est lié à aucun jeu")
+
+        session.commit()
+        print(f'{nb_users} utilisateur(s) créé(s) avec Faker')
+        print(f'{users_with_links} utilisateurs ont au moins 1 jeu ')
+        print(f"{nb_users - users_with_links} n'ont aucun jeu")
+    except SQLAlchemyError as e:
+        session.rollback()
+        print(f'Erreur lors de la création des utilisateurs et ou des liens avec Faker: {e}')
+    finally:
+        session.close()
+
+if __name__ == '__main__':
+    create_users_and_links()
+
+# 50 utilisateur(s) créé(s) avec Faker
+# 42 utilisateurs ont au moins 1 jeu 
+# 8 n'ont aucun jeu
+        
--- services/user_services.py
+++ services/user_services.py
@@ -33,8 +33,34 @@ def create_user(session):
     try:
         user = models.User(**data)
         session.add(user)
-        session.commit()
-        print(f'User {user.pseudo_user} créé')
+        session.flush()
+        
+        # user ayant des jeux ?
+        all_games = session.query(models.Game).all()
+        if all_games:
+            print("Choississez le ou les ID(s) des jeux que vous souhaitez ajouter au nouvel utilisateur")
+            for game in all_games: 
+                print(f'ID {game.id_game} - {game.name_game}')
+
+            choix = input('ID(s) séparés par une virgule ou vide si pas de jeu:').strip()
+
+            if choix:
+                ids = [int(x) for x in choix.split(',') if x.strip().isdigit()]
+                #nb_jeux_lies = 0
+
+                for game_id in ids:
+                    if any(g.id_game == game_id for g in all_games):
+                        link = models.User_Game(id_user = user.id_user, id_game = game_id)
+                        session.add(link)
+                        #nb_jeux_lies += 1
+                    else:
+                        print(f"Jeu ID {game_id} n'existe pas")
+
+                    #print(f'{nb_jeux_lies} Jeu(x) lié(s) ')
+            session.commit()
+        # user.games_links contient la liste des objets User_Game reliant user a jeux donc en recuperant sa length on a le nombre de jeux liés ne fonctionne pas avant de commit 
+        print(f'User {user.pseudo_user} créé avec {len(user.games_links)} jeu(x) lié(s)')
+
     except SQLAlchemyError as e:
         session.rollback()
         print(f"Erreur lors de l'ajout du nouvel user: {e}")
@@ -42,4 +68,81 @@ def create_user(session):
 def read_user():
     session = Session()
     try:
-        pseudo_user = input('Pseudo à afficher: ')
+        # Affichages possibles:
+        # tous les Users
+        # users avec au moins 1 jeu
+        # users liés à un jeu precis
+        # user par ID
+        # user par pseudo
+        print("\n=== Options d'affichage des Users ===")
+        print("1. Afficher tous les users")
+        print("2. Afficher les users avec au moins un jeu")
+        print("3. Afficher les users liés à un jeu précis")
+        print("4. Afficher un user par ID")
+        print("5. Afficher un user par pseudo")
+        choix = input("Choix (1-5) : ").strip()
+
+        if choix == '1':
+            # requete
+            users = session.query(models.User).all()
+            print(f'Liste des {len(users)} user(s)')
+            for u in users:
+                print(f'ID {u.id_user} - {u.pseudo_user} - nombre de jeu(x): {len(u.games_links)}')
+        elif choix =='2':
+            # requete
+            users = session.query(models.User).join(models.User_Game).distinct().all()
+            print(f'Liste des {len(users)} users ayant au moins un jeu')
+            for u in users:
+                print(f'ID {u.id_user} - {u.pseudo_user} - nombre de jeu(x): {len(u.games_links)} ')
+        elif choix == '3':
+            # requete
+            all_games = session.query(models.Game).all()
+            if not all_games:
+                print('Aucun jeu dans la base de données')
+                return
+            print('Liste des jeux: ')
+            for game in all_games:
+                print(f'ID {game.id_game} - {game.name_game}')
+            linked_game_str = input("Entrez l'ID du jeu: ").strip
+            if not linked_game_str.isdigit():
+                print('ID invalide')
+                return
+            linked_game_id = int(linked_game_str)
+
+            # requete
+            users = session.query(models.User).join(models.User_Game).filter(models.User_Game.id_game == linked_game_id).all()
+            linked_game = session.get(models.Game, linked_game_id)
+            linked_game_name = linked_game.name_game if linked_game else 'jeu inconnu'
+            print(f'{len(users)} user(s) lié(s) au jeu ID: {linked_game_id} - {linked_game_name}')
+            for u in users:
+                print(f"ID {u.id_user} | {u.pseudo_user}")
+        
+        elif choix =='4':
+            user_id_str = input("Entrez l'ID de l'User recherché").strip()
+            if not user_id_str.isdigit():
+                print('ID invalide')
+                return
+            user_id = int(user_id_str)
+            user = session.get(models.User, user_id)
+            if not user:
+                print('User non trouvé')
+                return
+            print(f'ID {user.id_user} - {user.pseudo_user} - lié à {len(user.games_links)} jeu(x)')
+            # affichage de la liste des jeux associés en suivant pour eviter la redondance du nom user et de l'id user 
+            for link in user.games_links:
+                print(f'{link.game.name_game}')
+        # a relire la suite        
+        elif choix == "5":
+            pseudo = input("Entrez le pseudo complet ou partiel : ").strip()
+            users = session.query(models.User).filter(models.User.pseudo_user.ilike(f"%{pseudo}%")).all()
+            print(f"\nUsers avec pseudo contenant '{pseudo}' ({len(users)}):")
+            for u in users:
+                print(f"ID {u.id_user} | {u.pseudo_user} | Jeux liés: {len(u.games_links)}")
+
+        else:
+            print("Choix invalide.")
+
+    except Exception as e:
+        print(f"Erreur lors de la lecture des users : {e}")
+    finally:
+        session.close()
