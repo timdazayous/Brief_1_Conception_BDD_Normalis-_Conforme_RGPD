86150452208f491b101758bf72b13fa43bb0c9af
timdazayous <tim.dazayous@gmail.com>
2025-12-02 12:07:05 +0100

    read_game done

--- services/game_services.py
+++ services/game_services.py
@@ -34,13 +34,13 @@ def input_game_data(session):
                 continue # on reboucle
 
             genre_temp = int(genre_str)
-            if any(g.id_genre == genre_temp for g in genres)
+            if any(g.id_genre == genre_temp for g in genres):
                 id_genre = genre_temp
                 break # sortie validée
             else:
                 print('ID invalide, inexistant')
     # publisher
-    publishers = session.query(models.Publisher)
+    publishers = session.query(models.Publisher).all
     id_publisher = None
 
     if not publishers:
@@ -72,3 +72,65 @@ def input_game_data(session):
         "id_genre_fk": id_genre,
         "id_publisher_fk": id_publisher
     }
+
+# Create
+def create_game():
+    session = Session()
+    try:
+        data = input_game_data(session)
+        game = models.Game(**data)
+        session.add(game)
+        session.commit()
+        print(f'Jeu {game.name_game} ID {game.id_game} créé ')
+    except SQLAlchemyError as e:
+        session.rollback()
+        print(('Erreur lors de la creation du jeu: {e}'))
+    finally:
+        session.clos()
+
+# Read
+def read_game():
+    session = Session()
+    try:
+        print("=== Options d'affichage des Jeux ===")
+        print("1. Tous les jeux")
+        print("2. Jeu par ID")
+        print("3. Jeux par nom (contient)")
+        choix = input("Choix (1-3) : ").strip()
+
+        if choix =='1':
+            games = session.query(models.Game).all()
+            # affichage de tous les jeux
+            print(f'Affichage de {len(games)} jeu(x)')
+            for g in games:
+                genre = g.genre.name_genre if g.genre else "N/A"
+                pub = g.publisher.name_publisher if g.publisher else "N/A"
+                print(f"ID {g.id_game} - Rank {g.rank_game} - Name {g.name_game} - Genre {genre} - Publisher {pub}")
+
+        elif choix =='2':
+            game_id_str = input('ID du jeu: ').strip()
+            if not game_id_str.isdigit():
+                print('ID invalide')
+                return
+            game = session.get(models.Game, int(game_id_str))
+            if not game:
+                print('ID du jeu non trouvée')
+                return
+            genre = game.genre.name_genre if game.genre else 'N/A'
+            pub = game.publisher.name_publisher if game.publisher else 'N/A'
+            print(f'ID {game.id_game} - Rank {game.rank_game} - Name {game.name_game} - Genre {genre} - Publisher {pub}')
+        
+        elif choix == '3':
+            # Nom du jeu ou nom proche pour la recherche recherche plus permissive 
+            name = input('Nom du jeu: ')
+            games = session.query(models.Game).filter(models.Game.name_game.ilike(f"%{name}%")).all()
+            print(f'{len(games)} jeu(x) trouvé(s): ')
+            for g in games:
+                print('ID {g.id_game} - {g.name_game}')
+        else:
+            print('choix invalide')
+    except Exception as e:
+        print(f'Erreur lors de la lecteur des jeux: {e}')
+    finally:
+        session.close()
+
