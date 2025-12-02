3bfaa9cee8e36e5ad4fb291e7ae64791bf2d3b6d
timdazayous <tim.dazayous@gmail.com>
2025-12-02 09:47:17 +0100

    CRUD user fonctionne meme si usine a gaz

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from sqlalchemy.exc import SQLAlchemyError
 from database import Session
 import models
+from services.user_services import create_user, read_user, delete_user, update_user
 
 def main():
     # ouverture d'une nouvelle session
@@ -9,12 +10,33 @@ def main():
     # bloc try execpt pour recuperer les erreurs lors de lexecution du main
     try:
         while True:
-        print("\n1. Créer un user")
-        print("2. Lire un user")
-        print("3. Modifier un user")
-        print("4. Supprimer un user")
-        print("0. Quitter")
-        choix = input("Choix : ")
+            print("1. Créer un user")
+            print("2. Lire un user")
+            print("3. Modifier un user")
+            print("4. Supprimer un user")
+            print("5. Quitter")
+            choix = input("Choix : ").strip()
 
-    except:
+            if choix == "1":
+                create_user(session)
+            elif choix == '2':
+                read_user()
+            elif choix == '3':
+                update_user()
+            elif choix == '4':
+                delete_user()
+            elif choix == '0':
+                print('fin')
+                break
+            else:
+                print('Choix invalide, réessayez')
+    except SQLAlchemyError as e:
+        print(f"Erreur SQLAlchemy dans le programme : {e}")
+    except Exception as e:
+        print(f"Erreur inattendue : {e}")
+    finally:
+        session.close()
+
+if __name__ == "__main__":
+    main()
 
