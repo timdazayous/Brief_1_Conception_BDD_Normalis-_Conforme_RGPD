dcb6441690f1930cb2fa0872cb9d29cc0302c172
timdazayous <tim.dazayous@gmail.com>
2025-12-02 16:08:04 +0100

    read_sale done

--- services/sale_services.py
+++ services/sale_services.py
@@ -40,11 +40,11 @@ def input_sale_data(session):
     glob = sale_amount('Global_sale')
 
     return {
-        'NA_Sale': na,
+        'NA_sale': na,
         'EU_sale': eu,
-        'JP_Sale': jp,
-        'Other_Sale': other,
-        'Global_Sale': glob,
+        'JP_sale': jp,
+        'Other_sale': other,
+        'Global_sale': glob,
         'id_game_fk': game_id
     }
     
@@ -64,3 +64,53 @@ def create_sale():
         print(f"Erreur lors de l'ajout de la sale: {e}")
     finally:
         session.close()
+
+# Read
+def read_sale():
+    session = Session()
+    try:
+        print("=== Options d'affichage des Sales ===")
+        print("1. Toutes les sales")
+        print("2. Sale par ID")
+        print("3. Sales d'un jeu (par ID de jeu)")
+        choix = input("Choix (1-3) : ").strip()
+
+        if choix == '1':
+            sales = session.query(models.Sale).all()
+            print(f'listes des {len(sales)} sales')
+            for s in sales:
+                game = s.game # on recupere lobjet complet grace la relation entre game et sale
+                name = game.name_game if game else 'N/A'
+                print(f'ID {s.id_sale} - Jeu {s.id_game_fk} {name} - NA_sales {s.NA_sale} - EU_sales {s.EU_sale} - JP_sales {s.JP_sale} - Other_sales {s.Other_sale} - Global_sales {s.Global_sale}')
+        elif choix == '2':
+            sale_id_str = input('ID de la sale: ').strip()
+            if not sale_id_str.isdigit():
+                print('ID invalide')
+                return
+            sale = session.get(models.Sale, int(sale_id_str))
+            if not sale:
+                print('ID invalide sale inexistante')
+                return
+            game = sale.game
+            name = game.name_game if game else 'N/A'
+            print(f'ID {sale.id_sale} - Jeu {sale.id_game_fk} {name} - NA_sales {sale.NA_sale} - EU_sales {sale.EU_sale} - JP_sales {sale.JP_sale} - Other_sales {sale.Other_sale} - Global_sales {sale.Global_sale}')
+        elif choix == '3':
+            game_id_str = input('ID du jeu: ').strip()
+            if not game_id_str.isdigit():
+                print('ID invalide')
+                return
+            game_id = int(game_id_str)
+            sales = session.query(models.Sale).filter(models.Sale.id_game_fk == game_id).all()
+            print(f'Liste des {len(sales)} sale(s) pour le jeu ID {game_id}')
+            for s in sales:
+                # normalement game existera tjr au vu des cardinalités et donc de la structure meme de ma bdd si je lai bien faite
+                game_name = s.game.name_game if s.game else 'N/A'
+                print(f'ID {s.id_sale} - Jeu {s.id_game_fk} {game_name} - NA_sales {s.NA_sale} - EU_sales {s.EU_sale} - JP_sales {s.JP_sale} - Other_sales {s.Other_sale} - Global_sales {s.Global_sale}')
+
+        else:
+            print('Choix invalide')
+    except Exception as e:
+        print(f'Erreur lors du read_sale: {e}')
+    finally:
+        session.close()
+
